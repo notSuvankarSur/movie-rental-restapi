75f25e83c5a684af8d1587532533456857920977
Suvankar Sur <sourav.sur74@gmail.com>
2020-05-12 02:39:13 +0530

    add support for connString via env var.

--- routes/movies.js
+++ routes/movies.js
@@ -6,25 +6,41 @@ const admin = require("../middleware/admin");
 const express = require("express");
 const router = express.Router();
 
-router.get(
-  "/",
-  asyncMiddleware(async (req, res) => {
-    const movies = await Movie.find().sort("title");
-    res.send(movies);
-  })
-);
+router.get("/", async (req, res) => {
+  const movies = await Movie.find().sort("title");
+  res.send(movies);
+});
 
-router.post(
-  "/",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+router.post("/", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
 
-    const genre = await Genre.findById(req.body.genreId);
-    if (!genre) return res.status(400).send("Invalid genre");
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre");
 
-    const movie = new Movie({
+  const movie = new Movie({
+    title: req.body.title,
+    genre: {
+      _id: genre._id,
+      name: genre.name,
+    },
+    numberInStock: req.body.numberInStock,
+    dailyRentalRate: req.body.dailyRentalRate,
+  });
+  await movie.save();
+  res.send(movie);
+});
+
+router.put("/:id", auth, async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  const genre = await Genre.findById(req.body.genreId);
+  if (!genre) return res.status(400).send("Invalid genre");
+
+  const movie = await Movie.findByIdAndUpdate(
+    req.params.id,
+    {
       title: req.body.title,
       genre: {
         _id: genre._id,
@@ -32,66 +48,32 @@ router.post(
       },
       numberInStock: req.body.numberInStock,
       dailyRentalRate: req.body.dailyRentalRate,
-    });
-    await movie.save();
-    res.send(movie);
-  })
-);
+    },
+    { new: true }
+  );
 
-router.put(
-  "/:id",
-  auth,
-  asyncMiddleware(async (req, res) => {
-    const { error } = validate(req.body);
-    if (error) return res.status(400).send(error.details[0].message);
+  if (!movie)
+    return res.status(404).send("The Movie with the given ID was not found.");
 
-    const genre = await Genre.findById(req.body.genreId);
-    if (!genre) return res.status(400).send("Invalid genre");
+  res.send(movie);
+});
 
-    const movie = await Movie.findByIdAndUpdate(
-      req.params.id,
-      {
-        title: req.body.title,
-        genre: {
-          _id: genre._id,
-          name: genre.name,
-        },
-        numberInStock: req.body.numberInStock,
-        dailyRentalRate: req.body.dailyRentalRate,
-      },
-      { new: true }
-    );
+router.delete("/:id", [auth, admin], async (req, res) => {
+  const movie = await Movie.findByIdAndRemove(req.params.id);
 
-    if (!movie)
-      return res.status(404).send("The Movie with the given ID was not found.");
+  if (!movie)
+    return res.status(404).send("The Movie with the given ID was not found.");
 
-    res.send(movie);
-  })
-);
+  res.send(movie);
+});
 
-router.delete(
-  "/:id",
-  [auth, admin],
-  asyncMiddleware(async (req, res) => {
-    const movie = await Movie.findByIdAndRemove(req.params.id);
+router.get("/:id", async (req, res) => {
+  const movie = await Movie.findById(req.params.id);
 
-    if (!movie)
-      return res.status(404).send("The Movie with the given ID was not found.");
+  if (!movie)
+    return res.status(404).send("The Movie with the given ID was not found.");
 
-    res.send(movie);
-  })
-);
-
-router.get(
-  "/:id",
-  asyncMiddleware(async (req, res) => {
-    const movie = await Movie.findById(req.params.id);
-
-    if (!movie)
-      return res.status(404).send("The Movie with the given ID was not found.");
-
-    res.send(movie);
-  })
-);
+  res.send(movie);
+});
 
 module.exports = router;
